d91d1409895a2704957fae24c5c425c78525c6f7
Marat <eshmakar@mail.ru>
2020-08-14 16:14:34 +0300

    Html files were added for all methods, program improvements

--- src/main/java/eshmakar/ru/myspringbootproject/controller/Contr.java
+++ src/main/java/eshmakar/ru/myspringbootproject/controller/Contr.java
@@ -39,52 +39,59 @@ public class Contr {
     }
 
 
-    /*//СТАРЫЙ КОД
-    //РАБОЧИЙ КОД
-    @GetMapping(value = "/for/{nu}" + "-" + "{nu2}") //диапазон цифр, например localhost:8080//100-500
-    public Object c0(@PathVariable long nu, @PathVariable long nu2) {
-        return new For().forEach(nu, nu2);
-    }*/
     @GetMapping(value = "/for/{nu}" + "-" + "{nu2}")
     public String returnDiapason(@PathVariable long nu, @PathVariable long nu2, Model model){
         model.addAttribute("returnDiapason", new For().forEach(nu, nu2));
         return "for";
     }
 
-
-
-
-
-
-
-
-
-
-    //возводит в степень. Пример: http://localhost:8080/15x5
-    @GetMapping(value = "/pow/{number}" + "x" + "{number2}") //при написании любых цифр, например localhost:8080//100x15
-    public Object c2(@PathVariable int number, @PathVariable int number2) {
-        return glavnayaStranica + new PowXpow().powXpow(number, number2);
-    }
-
-
-    @GetMapping("/random") //при переходе на random
-    public String random() throws URISyntaxException, IOException {
-        return glavnayaStranica + new Rand().randd() + new Rand().randd() + new Rand().randd() + new Rand().randd() + new Rand().randd() + new Rand().randd() + new Rand().randd() + new Rand().randd() + new Rand().randd() + new Rand().randd() + new Rand().randd()
-                + new Rand().randd() + new Rand().randd() + new Rand().randd() + new Rand().randd() + new Rand().randd() + new Rand().randd() + new Rand().randd() + new Rand().randd() + new Rand().randd() + new Rand().randd() + new Rand().randd() + new Rand().randd();
-
+    //возводит в степень. Пример: http://localhost:8080/12x34
+    @GetMapping(value = "/pow/{number}" + "x" + "{number2}")
+    public String powXPow(@PathVariable int number, @PathVariable int number2, Model model){
+        model.addAttribute("returnPowXPow", new PowXpow().powXpow(number, number2));
+        return "powXpow";
     }
 
     @GetMapping(value = "/reverse/{name}")
-    public String reverse(@PathVariable StringBuffer name) {
+    public String returnReverse(@PathVariable StringBuffer name, Model model){
         StringBuffer stringBuffer = new StringBuffer(name);
         StringBuffer stringBuffer2 = new StringBuffer(name);
-        return "Исходный текст: " + "<br>" + stringBuffer + "<br>" + "<br>" + "<br>" + "Текст наоборот (reverse): " + "<br>" + stringBuffer2.reverse();
+        model.addAttribute("world", stringBuffer);
+        model.addAttribute("returnReverse", stringBuffer2.reverse());
+        return "reverse";
     }
 
-
+    /*//СТАРЫЙ КОД
     @GetMapping(value = "/case/{name}")
     public String casee(@PathVariable String name) {
         String text = name;
         return "Исходный текст: " + "<br>" + text + "<br>" + "<br>" + "<br>" + "прописные буквы: " + "<br>" + text.toLowerCase() + "<br>" + "<br>" + "<br>" + "ЗАГЛАВНЫЕ БУКВЫ: " + "<br>" + text.toUpperCase();
+    }*/
+    @GetMapping(value = "/case/{name}")
+    public String returnCase(@PathVariable String name, Model model){
+        String text = name;
+        model.addAttribute("original_text", text);
+        model.addAttribute("toLowerCase", text.toLowerCase());
+        model.addAttribute("toUpperCase", text.toUpperCase());
+        return "case";
     }
+
+    /*//СТАРЫЙ КОД
+    @GetMapping("/random") //при переходе на random
+    public String random() throws URISyntaxException, IOException {
+        return glavnayaStranica + new Rand().randd() + new Rand().randd() + new Rand().randd() + new Rand().randd() + new Rand().randd() + new Rand().randd() + new Rand().randd() + new Rand().randd() + new Rand().randd() + new Rand().randd() + new Rand().randd()
+                + new Rand().randd() + new Rand().randd() + new Rand().randd() + new Rand().randd() + new Rand().randd() + new Rand().randd() + new Rand().randd() + new Rand().randd() + new Rand().randd() + new Rand().randd() + new Rand().randd() + new Rand().randd();
+    }*/
+
+    @GetMapping("random")
+    public String randomList(Model model){
+        for (int i = 0; i < 21; i++) {
+            model.addAttribute("random" +i, new Rand().randd());
+        }
+        return "random";
+    }
+
+
+
+
 }
